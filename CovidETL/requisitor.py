beca38d6dde4d0fdc2f13787d5fa17c94be8dda8
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-06-08 22:24:46 -0300

    adicionando class, ajustando condição API ok

---
+++ CovidETL/requisitor.py
@@ -0,0 +1,79 @@
+from pytz import country_names
+import requests
+from datetime import datetime
+import logging
+import sys
+# import pydantic
+
+logger = logging.getLogger(__name__) #contém o nome do módulo
+logger.setLevel(logging.INFO) #seta mensagens de info (erros, warnings, critical)
+
+
+# state = country_names
+# for y,x in state.items():
+#     print(x)
+class CovidBrasil:
+    def requisita_estados(states=None):
+        # Lista casos por estado
+        request = requests.get('https://covid19-brazil-api.now.sh/api/status/v1')
+        if request.status_code != 200:
+            logger.warning('API fora do ar.')
+            sys.exit()
+
+        request = requests.get(f'https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/{states}').json()
+
+        # Parser
+        id = request['uid']
+        uf = request['uf']
+        state = request['state']
+        cases = request['cases']
+        deaths = request['deaths']
+        suspects = request['suspects']
+        refuses = request['refuses']
+        datenow = request['datetime'].split('T')[0]
+        datenow = datetime.fromisoformat(datenow).strftime('%d-%m-%Y')
+        requested_list = (id,uf,state, cases, deaths, suspects, refuses, datenow)
+        return requested_list
+
+
+
+
+class CovidMundo:
+    def requisita_paises(country=None):
+        # Lista de casos por país
+        request = requests.get('https://covid19-brazil-api.now.sh/api/status/v1')
+        if request.status_code != 200:
+            logger.warning('API fora do ar.')
+            sys.exit()
+
+        request = requests.get(f'https://covid19-brazil-api.now.sh/api/report/v1/{country}').json()
+
+        # Parser
+        item = request['data']
+        country = item['country']
+        confirmed = item['confirmed']
+        deaths = item['deaths']
+        datenow = item['updated_at'].split('T')[0]
+        datenow = datetime.fromisoformat(datenow).strftime('%d-%m-%Y')
+        requested_list1 = (country, confirmed, deaths, datenow)
+        return requested_list1
+
+# '''(PADRÃO) Lista casos por todos estados brasileiros
+# https://covid19-brazil-api.now.sh/api/report/v1'''
+# r = requests.get('https://covid19-brazil-api.now.sh/api/report/v1').json()
+
+
+
+
+
+# '''Lista casos no brasil em data específica.
+# https://covid19-brazil-api.now.sh/api/report/v1/brazil/20200318
+# r = requests.get('https://covid19-brazil-api.now.sh/api/report/v1/brazil/20200318').json()'''
+# r = requests.get(f'https://covid19-brazil-api.now.sh/api/report/v1/{self.country}/{self.date}').json()
+
+
+
+# '''Consultar status da API
+# https://covid19-brazil-api.now.sh/api/status/v1'''
+# r = requests.get('https://covid19-brazil-api.now.sh/api/status/v1').json()
+
---
+++ CovidETL/write.py
@@ -0,0 +1,23 @@
+import logging
+import csv
+
+
+
+class writer():
+    HEADER_STATES = ('Id', 'UF', 'Estado', 'Casos confirmados', 'Mortes', 'Suspeitos', 'Negativados', 'Monitorado até' )
+    HEADER_COUNTRY = ('País', 'Casos confirmados', 'Mortes', 'Atualizado até')
+
+    @classmethod
+    def write_brStates(cls, archive_name=None, header=HEADER_STATES, contain=None):
+        with open(f'casos_{archive_name}.csv', 'w') as file:
+            wr = csv.writer(file, delimiter=',')
+            wr.writerow(header)
+            wr.writerow(contain)
+
+    
+    @classmethod
+    def write_allCountries(cls, archive_name=None, header=HEADER_COUNTRY,contain=None):
+        with open(f'dados_{archive_name}.csv', 'w') as file:
+            wr = csv.writer(file, delimiter=',')
+            wr.writerow(header)
+            wr.writerow(contain)
