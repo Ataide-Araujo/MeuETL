5f7f6d2a1196aa966a59fda2f0077f2bdde77d78
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-06-11 15:41:09 -0300

    ajustando o main pra rodar por args

--- CovidETL/__main__.py
+++ CovidETL/__main__.py
@@ -1,15 +1,21 @@
 from CovidETL import CovidBrasil, CovidMundo
 from CovidETL import writer
+import argparse
 
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-estado","--estado", help="Informe a sigla do estado", type=str)
+    parser.add_argument("-pais","--pais", help="Informe o nome do país", type=str)
+    args = parser.parse_args()
+  
 
+    if args.estado:
+        a = CovidBrasil.requisita_estados(states=args.estado)
+        writer.write_brStates(archive_name=args.estado,contain=a)
+    elif args.pais:
 
-    a = CovidBrasil.requisita_estados('sp')
-    writer.write_brStates(archive_name='sp',contain=a)
-
-
-    # b = CovidMundo
-    # d = b.requisita_paises('cuba')
-    # writer.write_allCountries(contain=d)
+        b = CovidMundo
+        d = b.requisita_paises(country=args.pais)
+        writer.write_allCountries(archive_name=args.pais,contain=d)
