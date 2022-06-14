0dee42ee0dc8d1a1f1e7dd2862eb33b7be7d3620
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-06-13 21:55:24 -0300

    ajustando main/ add all country

--- CovidETL/__main__.py
+++ CovidETL/__main__.py
@@ -1,21 +1,32 @@
+import logging
 from CovidETL import CovidBrasil, CovidMundo
 from CovidETL import writer
 import argparse
-
+from pytz import country_names
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-estado","--estado", help="Informe a sigla do estado", type=str)
     parser.add_argument("-pais","--pais", help="Informe o nome do país", type=str)
+    parser.add_argument("--all", help="Pega todos os países da lista", action="store_true")
     args = parser.parse_args()
   
 
     if args.estado:
-        a = CovidBrasil.requisita_estados(states=args.estado)
-        writer.write_brStates(archive_name=args.estado,contain=a)
+        state = CovidBrasil.requisita_estados(states=args.estado)
+        writer.write_brStates(archive_name=args.estado,contain=state)
     elif args.pais:
 
-        b = CovidMundo
-        d = b.requisita_paises(country=args.pais)
-        writer.write_allCountries(archive_name=args.pais,contain=d)
+        country = CovidMundo
+        dados = country.requisita_paises(country=args.pais)
+        writer.write_allCountries(archive_name=args.pais,contain=dados)
+    
+    elif args.all:
+        state = country_names
+        for sigla, pais in state.items():
+            pais = CovidMundo.RepCountry(pais)
+            print(f'Gravando o país: {pais}')
+            country = CovidMundo(pais)
+            oneCountry = country.requisita_paises()
+            writer.write_allCountries(archive_name=pais, contain=oneCountry)
