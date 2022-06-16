67f84ced481fe18f4f3435f4eeed585007d06962
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-06-16 13:41:40 -0300

    fix 2 args cmd

--- CovidETL/argparse.py
+++ CovidETL/argparse.py
@@ -3,8 +3,7 @@ from argparse import ArgumentParser
 class Args():
     def args_params():
         parser = ArgumentParser()
-        parser.add_argument("-estado","--estado", help="Informe a sigla do estado", type=str)
-        parser.add_argument("-pais","--pais", help="Informe o nome do país", type=str)
-        parser.add_argument("--all", help="Pega todos os países da lista", action="store_true")
+        parser.add_argument("-estado", help="Traz todos os estados do Brasil", action="store_true")
+        parser.add_argument("-pais", help="Traz todos os países do Mundo", action="store_true")
         args = parser.parse_args()
         return args
