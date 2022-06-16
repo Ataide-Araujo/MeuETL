51627cbbf6f6914bb391e11285480e913807a15c
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-06-15 21:52:26 -0300

    add imports

--- CovidETL/__init__.py
+++ CovidETL/__init__.py
@@ -1,2 +1,4 @@
-from .requisitor import CovidBrasil, CovidMundo
+from .requisitor import CovidBrasil, CovidMundo, Check
+from .argparse import Args
 from .write import writer
+from .group_country import Group
