8e6698a2a4ad542d1f23d4e59d0260f66f788f59
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-06-26 16:14:04 -0300

    ajustes nos nomes das variaveis

--- CovidETL/__main__.py
+++ CovidETL/__main__.py
@@ -3,23 +3,24 @@ from CovidETL import writer
 from CovidETL import Args
 from CovidETL import MakeGraph
 
+
 def main():
     args = Args.args_params()
 
     if Check.check_api():
         if args.estado:
-            archive_name='casos_Todos_estados'
+            file_name='casos_Todos_estados'
             state = CovidBrasil
             dados = state.requisita_estados()
-            writer.write_brStates(archive_name=archive_name,contain=dados)
-            MakeGraph.processState(f'{archive_name}.csv')
+            writer.write_brStates(file_name=file_name,contain=dados)
+            MakeGraph.processState(f'{file_name}.csv')
 
         elif args.pais:
-            archive_name='casos_Todos_paises'
+            file_name='casos_Todos_paises'
             country = CovidMundo
             dados = country.requisita_paises()
-            writer.write_allCountries(archive_name=archive_name,contain=dados)
-            MakeGraph.processCountry(f'{archive_name}.csv')
+            writer.write_allCountries(file_name=file_name,contain=dados)
+            MakeGraph.processCountry(f'{file_name}.csv')
 
 
 if __name__ == '__main__':
