b0b3f4dad170236f4c3157a7cd102c467260c308
Ataide-Araujo <ataide.nascimento@nappsolutions.com>
2022-06-26 16:16:09 -0300

    organizando classe/add criação de pastas proprias

--- CovidETL/write.py
+++ CovidETL/write.py
@@ -1,17 +1,22 @@
 import logging
 import csv
+import os
 
 logger = logging.getLogger(__name__) #contém o nome do módulo
 logger.setLevel(logging.INFO) #seta mensagens de info (erros, warnings, critical)
 logging.basicConfig(level=logging.INFO)
 
-class writer():
+
+class writer:
     HEADER_STATES = ('Id', 'UF', 'Estado', 'Casos confirmados', 'Mortes', 'Suspeitos', 'Negativados', 'Monitorado até' )
     HEADER_COUNTRY = ('País', 'Casos confirmados', 'Mortes', 'Atualizado até')
 
     @classmethod
-    def write_brStates(cls, archive_name=None, header=HEADER_STATES, contain=None):
-        with open(f'{archive_name}.csv', 'w') as file:
+    def write_brStates(cls, file_name=None, header=HEADER_STATES, contain=None):
+        if not os.path.exists(f'{os.getcwd()}/{file_name}'):
+            os.makedirs(f'{os.getcwd()}/{file_name}')
+        os.chdir(f'{os.getcwd()}/{file_name}')
+        with open(f'{file_name}.csv', 'w') as file:
             wr = csv.writer(file, delimiter=',')
             wr.writerow(header)
             for item in contain:
@@ -20,8 +25,11 @@ class writer():
 
     
     @classmethod
-    def write_allCountries(cls, archive_name=None, header=HEADER_COUNTRY,contain=None):
-        with open(f'{archive_name}.csv', 'w') as file:
+    def write_allCountries(cls, file_name=None, header=HEADER_COUNTRY,contain=None):
+        if not os.path.exists(f'{os.getcwd()}/{file_name}'):
+            os.makedirs(f'{os.getcwd()}/{file_name}')
+        os.chdir(f'{os.getcwd()}/{file_name}')
+        with open(f'{file_name}.csv', 'w') as file:
             wr = csv.writer(file, delimiter=',')
             wr.writerow(header)
             for item in contain:
